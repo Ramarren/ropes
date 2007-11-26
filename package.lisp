9daae9e81dbfeea0474e278cbc0e2c86b34f03d1
Ramarren <ramarren@gmail.com>
2007-11-26 17:33:31 +0100

    Export symbols.

--- package.lisp
+++ package.lisp
@@ -1,3 +1,5 @@
 (defpackage :ropes
   (:use :cl :iterate)
-  (:import-from #:arnesi #:aif #:it #:rcurry #:curry))
+  (:import-from #:arnesi #:aif #:it #:rcurry #:curry)
+  (:export #:make-rope #:rope-walk #:rope-concat #:rope-to-string #:rope-elt #:make-rope-iterator
+	   #:do-rope #:rope-substring))
